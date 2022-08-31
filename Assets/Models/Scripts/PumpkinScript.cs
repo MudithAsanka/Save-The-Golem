6db357050a682e7483231d4ce73a25d649f4ea2b
Mudith Asanka <mudithasanka.dev@gmail.com>
2022-09-01 02:35:59 +0530

    Destroy pumpkins after pass the golem

--- Assets/Models/Scripts/PumpkinScript.cs
+++ Assets/Models/Scripts/PumpkinScript.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class PumpkinScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    void Update()
     {
-        
+        // Destory pumpkins after pass the golem
+        if(transform.position.y < -3f)  
+        {
+            Destroy(gameObject);
+        }
     }
 
+    // Game Pause
     void OnCollisionEnter(Collision target)
     {
         if(target.gameObject.tag == "Golem")
